f5a7eb68b56ef2437b17ca1680305cd0465b00bc
hrdwrrsk <noegraha-poernama@outlook.com>
2020-02-20 14:20:48 +0700

    gtk2: add clean-assets.sh (2)
    properly delete asset file

--- AdwaitaEx-dark/gtk-2.0/clean-assets-external.sh
+++ AdwaitaEx-dark/gtk-2.0/clean-assets-external.sh
@@ -5,6 +5,6 @@ INDEX="assets-external.txt"
 for i in `cat $INDEX`
 do
 	echo Deleting $i.png
-	rm -f $i.png
+	rm -f assets/$i.png
 done
 exit 0
--- AdwaitaEx-dark/gtk-2.0/clean-assets.sh
+++ AdwaitaEx-dark/gtk-2.0/clean-assets.sh
@@ -5,6 +5,6 @@ INDEX="assets.txt"
 for i in `cat $INDEX`
 do
 	echo Deleting $i.png
-	rm -f $i.png
+	rm -f assets/$i.png
 done
 exit 0
